3bb7f6e4568d919e6eab8d3d6a0826aa721ad1ea
Gagan <gagansasan91@yahoo.com>
2024-10-22 22:41:40 -0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
     return (
         <>
             <NavBar></NavBar>
-            
             <h1 className="text-xl bg-">Vite + React</h1>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
