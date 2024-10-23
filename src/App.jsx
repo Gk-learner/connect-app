6ddffa91a1f61e6aa51d1955f477f87d7035e51c
Gagan <gagansasan91@yahoo.com>
2024-10-22 22:36:43 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <>
+            <NavBar></NavBar>
+            
+            <h1 className="text-xl bg-">Vite + React</h1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
